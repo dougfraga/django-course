c10f48dfd0d634d51354baf17c0ee6530817af8b
dougfraga <douglas.fraga@gmail.com>
2020-10-23 13:09:40 -0300

    Migrations added.

--- pypro/modules/admin.py
+++ pypro/modules/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from ordered_model.admin import OrderedModelAdmin
 
-from pypro.modules.models import Module
+from pypro.modules.models import Module, Lesson
 
 
 @admin.register(Module)
 class ModuleAdmin(OrderedModelAdmin):
     list_display = ('title', 'public', 'move_up_down_links')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Lesson)
+class LessonAdmin(OrderedModelAdmin):
+    list_display = ('title', 'module', 'order', 'move_up_down_links')
+    list_filter = ('module',)
+    ordering = ('module', 'order')
     prepopulated_fields = {'slug': ('title',)}
